a801dbd3345b909455f920c54aff8b62c1a2db61
ThiagoNeckel <thiagonneckel@gmail.com>
2021-07-02 11:06:33 -0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,4 +1,8 @@
 lista= ['ai',1,3,'oi',3.0,'10,00', True];
 
-for i , obj in enumerate(lista):
-    print(i, obj);
+cont = 0
+for item in lista:
+
+    if item != 0:
+        cont = cont + 1
+        print(cont, item)
